90fd2f376169b9987829b2e14b65969b018dc08c
ArthurGaleev <113104318+ArthurGaleev@users.noreply.github.com>
2025-07-02 02:13:13 +0300

    Update top_lliked_pop_usa.sql

--- requests/top_lliked_pop_usa.sql
+++ requests/top_lliked_pop_usa.sql
@@ -3,8 +3,8 @@ SELECT a.name AS artist,
        COUNT ( * ) likes,
        s.popularity num_of_plays
 FROM Liked_songs ls
-JOIN Songs s ON s.artist_id = ls.song_id
+JOIN Songs s ON s.id = ls.song_id
 JOIN Artists a ON a.id = s.artist_id
 JOIN Countries c ON c.id = a.country_id AND c.name = 'USA'
 GROUP BY 1, 2, 4
-ORDER BY 3 DESC, 4 DESC, 1 DESC, 2 DESC;
+ORDER BY 3 DESC, 4 DESC, 1, 2;
